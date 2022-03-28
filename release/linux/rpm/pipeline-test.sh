7867605ec170ae8941b0464d31ded0cab006c4a4
Stuart Padley <stuartpa@hotmail.com>
2022-03-28 14:07:23 -0700

    Update file header

--- release/linux/rpm/pipeline-test.sh
+++ release/linux/rpm/pipeline-test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 #------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
 #------------------------------------------------------------------------------
 
 # Description:
--- release/linux/rpm/pipeline.sh
+++ release/linux/rpm/pipeline.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-
 #------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
 #------------------------------------------------------------------------------
 
 # Description:
