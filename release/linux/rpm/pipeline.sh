3fbb94dbe390d0a5c3ab275e11a7c00fa15601e3
Stuart Padley <stuartpa@microsoft.com>
2022-04-04 05:43:25 -0700

    Ensure artifact is available

--- release/linux/rpm/pipeline.sh
+++ release/linux/rpm/pipeline.sh
@@ -21,6 +21,8 @@ set -exv
 
 : "${REPO_ROOT_DIR:=`cd $(dirname $0); cd ../../../; pwd`}"
 
+ls -l ${BUILD_OUTPUT}
+
 cp ${BUILD_OUTPUT}/SqlcmdLinuxAmd64/sqlcmd ${REPO_ROOT_DIR}/sqlcmd
 
 DIST_DIR=${BUILD_STAGINGDIRECTORY:=${REPO_ROOT_DIR}/output/rpm}
