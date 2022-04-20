9e65bfcdd88f06ea7a3ee31d3bbf655635295f3e
Stuart Padley <stuartpa@microsoft.com>
2022-04-20 11:21:47 -0700

    Merge conflicts

--- release/linux/docker/pipeline-test.sh
+++ release/linux/docker/pipeline-test.sh
@@ -19,16 +19,6 @@ set -exv
 
 : "${REPO_ROOT_DIR:=`cd $(dirname $0); cd ../../../; pwd`}"
 
-<<<<<<< HEAD
-PACKAGE_VERSION=${PACKAGE_VERSION:=0.0.1}
-
-BUILD_ARTIFACTSTAGINGDIRECTORY=${BUILD_ARTIFACTSTAGINGDIRECTORY:=${REPO_ROOT_DIR}/output/docker}
-IMAGE_NAME=microsoft/sqlcmd${BUILD_BUILDNUMBER:=''}:latest
-TAR_FILE=${BUILD_ARTIFACTSTAGINGDIRECTORY}/docker-sqlcmd-${PACKAGE_VERSION}.tar
-
-echo "=========================================================="
-echo "PACKAGE_VERSION: ${PACKAGE_VERSION}"
-=======
 PACKAGE_VERSION=${CLI_VERSION:=0.0.1}
 PACKAGE_VERSION_REVISION=${CLI_VERSION_REVISION:=1}
 
@@ -39,15 +29,10 @@ TAR_FILE=${BUILD_ARTIFACTSTAGINGDIRECTORY}/sqlcmd-docker-${PACKAGE_VERSION}-${PA
 echo "=========================================================="
 echo "PACKAGE_VERSION: ${PACKAGE_VERSION}"
 echo "PACKAGE_VERSION_REVISION: ${PACKAGE_VERSION_REVISION}"
->>>>>>> main
 echo "BUILD_ARTIFACTSTAGINGDIRECTORY: ${BUILD_ARTIFACTSTAGINGDIRECTORY}"
 echo "Image name: ${IMAGE_NAME}"
 echo "Docker image file: ${TAR_FILE}"
 echo "=========================================================="
 
 docker load < ${TAR_FILE}
-<<<<<<< HEAD
-docker run ${IMAGE_NAME} /bin/bash -c "/sqlcmd --help" || exit 1
-=======
 docker run ${IMAGE_NAME} sqlcmd --help || exit 1
->>>>>>> main
